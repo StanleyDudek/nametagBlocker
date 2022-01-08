4e1213a9cbc372a91e6dccbbb2c7fb77715556b4
Dudekahedron <49531350+StanleyDudek@users.noreply.github.com>
2022-01-08 16:58:22 -0500

    Rewrite to support server-side whitelist

--- lua/ge/extensions/nametagBlocker.lua
+++ lua/ge/extensions/nametagBlocker.lua
@@ -2,18 +2,71 @@
 
 local M = {}
 
-local function onPreRender(dt)
-	MPVehicleGE.hideNicknames(true)
+local whitelisted = false
+local nametagBlockerActive = false
+local nametagBlockerTimeout
+
+local function rxWhitelist(data)
+	if data == "false" then
+		whitelisted = false
+	elseif data == "true" then
+		whitelisted = true
+	end
+end
+
+local function rxNametagBlockerActive(data)
+	if data == "false" then
+		nametagBlockerActive = false
+	elseif data == "true" then
+		nametagBlockerActive = true
+	end
+end
+
+local function rxNametagBlockerTimeout(data)
+	if tonumber(data) == 0 then
+		nametagBlockerTimeout = nil
+	else
+		nametagBlockerTimeout = tonumber(data)
+	end
 end
 
 local function onExtensionLoaded()
-	MPVehicleGE.hideNicknames(true)
-	log("M","nametagBlocker","Loading nametagBlocker")
+	log("D","nametagBlocker","Loading nametagBlocker")
+	AddEventHandler("rxWhitelist", rxWhitelist)
+	AddEventHandler("rxNametagBlockerActive", rxNametagBlockerActive)
+	AddEventHandler("rxNametagBlockerTimeout", rxNametagBlockerTimeout)
+	log("D","nametagBlocker","nametagBlocker Loaded")
 end
 
 local function onExtensionUnloaded()
+	log("D","nametagBlocker","Unloading nametagBlocker")
 	Lua:requestReload()
-	log("M","nametagBlocker","Unloading nametagBlocker")
+	log("D","nametagBlocker","nametagBlocker Unoaded")
+end
+
+local function onPreRender(dt)
+	if nametagBlockerActive then
+		if nametagBlockerTimeout ~= nil then
+			nametagBlockerTimeout = nametagBlockerTimeout - dt
+			if nametagBlockerTimeout > 0 then
+				if not whitelisted then
+					MPVehicleGE.hideNicknames(true)
+				else
+					MPVehicleGE.hideNicknames(false)
+				end
+			else
+				nametagBlockerTimeout = nil
+			end
+		else
+			if not whitelisted then
+				MPVehicleGE.hideNicknames(true)
+			else
+				MPVehicleGE.hideNicknames(false)
+			end
+		end
+	else
+		MPVehicleGE.hideNicknames(false)
+	end
 end
 
 M.onPreRender = onPreRender
